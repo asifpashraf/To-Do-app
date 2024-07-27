8dec3f25ced8ed6e6f7f7b4dfea368c75c147372
asif p ashraf <asifpashraf@gmail.com>
2024-07-27 08:22:07 +0530

    second commit

--- script.js
+++ script.js
@@ -5,21 +5,44 @@ const completed = document.getElementById("completed")
 
 function createTask(){
     if(one.value==''){
-        alert("write somethimg")
+        alert("write something")
     }
-    else if(one.value!==''){
+    else      {
+        
         // alert("you are moving forward")
+        
+        //  created elemente
         const li = document.createElement('li')
-        li.id="myLi"
         const createcheck = document.createElement('input')
-        createcheck.type ="checkBox"
-        createcheck.id="myCheck"
+        const deleted = document.createElement('button')
+        const edit = document.createElement('button')
+
+        
+
+    //event listener
+        deleted.addEventListener("click",function(){
+            myList.removeChild(li)
+        })
+        createcheck.addEventListener("click",function(){
+            const complet = document.getElementById('completed')
+            complet.appendChild(li)
+            
+        })
+        // create text content
+
         li.textContent =one.value
+        deleted.textContent ="delete"
+        edit.textContent ="edit"
+        
+        createcheck.type ="checkBox"
+        
+        // append child for li
         myList.appendChild(li)
         li.appendChild(createcheck)
+        li.appendChild(edit)
+        li.appendChild(deleted)
         one.value = ''
-        
     }
-    return li
+
 }
 
--- script2.js
+++
@@ -1,5 +0,0 @@
-//add task to the uncompleted tasks
-
-//add task to completed task from un completed task
-
-//add delete and edit button to the tasks
